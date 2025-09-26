e96de1ad84c3c718b3716e7b11bb9cd7276a719a
choiruzadit-code <choiruz12adit@gmail.com>
2025-09-26 19:53:34 +0700

    Add files via upload

---
+++ tugas2.py
@@ -0,0 +1,6 @@
+#tugas 2
+judul_buku = str(input("judul buku :"))
+penulis = str(input("Penulis :"))
+Tahun_terbit = str(input("Tahun terbit :"))
+
+print (f"\nbuku dengan judul :{judul_buku}  \nditulis oleh :{penulis} \nTahun terbit :{Tahun_terbit}")
