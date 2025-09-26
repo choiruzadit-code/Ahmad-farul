816464a62d60ea403fb3a5849a5f23d69323e154
choiruzadit-code <choiruz12adit@gmail.com>
2025-09-26 19:56:34 +0700

    Add files via upload

---
+++ tugas3.py
@@ -0,0 +1,6 @@
+#tugas 3
+nama_depan = str(input("Masukan nama depan :"))
+nama_belakang = str(input("Masukan nama belakang :"))
+usia = str(input("Masukan usia anda :"))
+
+print(f"\nPerkenalkan nama saya{nama_depan} {nama_belakang}\nsaya berusia{usia}  tahun")
