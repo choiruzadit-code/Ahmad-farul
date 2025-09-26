e44ba58d02905e555eba77cfc02a034efe91b438
choiruzadit-code <choiruz12adit@gmail.com>
2025-09-26 19:55:26 +0700

    Add files via upload

---
+++ tugas1.py
@@ -0,0 +1,8 @@
+# tugas 1
+nama = input("Masukan nama anda :")
+tahun_lahir = int(input("Masukan tahun lahir anda :"))
+
+tahun_sekarang = 2025
+umur = tahun_lahir - tahun_sekarang
+
+print(f"Halo{nama} umur kamu sekarang{umur} tahun.")
